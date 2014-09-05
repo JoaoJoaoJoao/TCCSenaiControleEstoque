72c45a111b527266141f450fc1048ee8bc64d902
joao_victor1 <joao_victor1@SNSOO1EP01VM025.saojose.sc.senai.br>
2014-09-05 19:11:17 -0300

    Criando

---
+++ ProjetoTCC/src/java/entity/Produto.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entity;
+
+/**
+ *
+ * @author joao_victor1
+ */
+public class Produto {
+    
+}
